9c55380ee786d6f02469fbd95fb87fe4fb8c8a1c
Bratipah <bratipahmh@gmail.com>
2024-03-20 17:05:32 +0300

    update landing page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
 import NavBar from './components/navigation/NavBar.vue'
+import HomePage from './components/homepage/HomePage.vue';
 
 </script>
 
 <template>
-  <NavBar/>
+ <NavBar/> 
+ <HomePage/>
+
+
 </template>
 
 
--- src/components/homepage/HomePage.vue
+++ src/components/homepage/HomePage.vue
@@ -1,5 +1,134 @@
 <template>
-    <div>
-        
+  <section class="bg-cyan-200">
+    <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+      <div class="mr-auto place-self-center lg:col-span-7">
+        <h1
+          class="max-w-2xl mb-4 text-4xl font-extrabold leading-none md:text-5xl xl:text-7xl text-blue-900"
+        >
+          Your Partner in Health & wellness
+        </h1>
+        <p
+          class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl font-normal"
+        >
+          We are committed to providing you with the best medical and healthcare services to help
+          you live healthier and happier..
+        </p>
+        <a
+          href="#"
+          class="inline-flex items-center justify-center px-5 py-2 mr-3 text-base font-medium text-center text-black-200 rounded-full hover:bg-blue-800 hover:text-white focus:ring-transparent focus:ring-blue-300 hover:fill-white"
+        >
+          <svg
+            class="w-5 h-8 -mr-1 fill-blue-500 hover:fill-white"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+          <span class="indent-3"> See How we Work</span>
+        </a>
+      </div>
+      <div class="hidden lg:mt-25 lg:col-span-5 lg:flex">
+        <img
+          class="h-auto w-auto bg-cyan-200"
+          src="../../assets/images/2doctors-removebg-preview.png"
+          alt="doctors"
+        />
+      </div>
     </div>
+  </section>
+
+  <!-- DateSection Book Now -->
+
+  <section>
+   <div class="flex md:flex-row rounded-md shadow-blue-500 p-5 m-10">
+        <div class="flex">
+          <a href="">
+            <svg
+              class="h-8 w-10 fill-blue-500"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+            >
+              <path
+                d="M128 0c17.7 0 32 14.3 32 32V64H288V32c0-17.7 14.3-32 32-32s32 14.3 32 32V64h48c26.5 0 48 21.5 48 48v48H0V112C0 85.5 21.5 64 48 64H96V32c0-17.7 14.3-32 32-32zM0 192H448V464c0 26.5-21.5 48-48 48H48c-26.5 0-48-21.5-48-48V192zm64 80v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm128 0v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H208c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V272c0-8.8-7.2-16-16-16H336zM64 400v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H80c-8.8 0-16 7.2-16 16zm144-16c-8.8 0-16 7.2-16 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H208zm112 16v32c0 8.8 7.2 16 16 16h32c8.8 0 16-7.2 16-16V400c0-8.8-7.2-16-16-16H336c-8.8 0-16 7.2-16 16z"
+              />
+            </svg>
+            <div class="relative">
+                <p class="ml-16 truncate text-sm font-medium text-gray-900">Date</p>
+            <p class="ml-16 truncate text-sm font-medium text-gray-800">August 23, 2023</p>
+            </div>
+           
+          </a>
+        </div>
+        <div class="flex">
+          <a href="">
+            <svg
+              class="h-8 w-10 fill-blue-500"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 576 512"
+            >
+              <path
+                d="M142.4 21.9c5.6 16.8-3.5 34.9-20.2 40.5L96 71.1V192c0 53 43 96 96 96s96-43 96-96V71.1l-26.1-8.7c-16.8-5.6-25.8-23.7-20.2-40.5s23.7-25.8 40.5-20.2l26.1 8.7C334.4 19.1 352 43.5 352 71.1V192c0 77.2-54.6 141.6-127.3 156.7C231 404.6 278.4 448 336 448c61.9 0 112-50.1 112-112V265.3c-28.3-12.3-48-40.5-48-73.3c0-44.2 35.8-80 80-80s80 35.8 80 80c0 32.8-19.7 61-48 73.3V336c0 97.2-78.8 176-176 176c-92.9 0-168.9-71.9-175.5-163.1C87.2 334.2 32 269.6 32 192V71.1c0-27.5 17.6-52 43.8-60.7l26.1-8.7c16.8-5.6 34.9 3.5 40.5 20.2zM480 224a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"
+              />
+            </svg>
+        
+            <div class="relative">
+                <p class="ml-4 truncate text-sm font-medium text-gray-900">Specialization</p>
+              <input type="checkbox" id="sortbox" class="hidden absolute" />
+              <label for="sortbox" class="flex items-center space-x-1 cursor-pointer">
+                <span class="text-sm indent-3">Cardiology</span>
+                <svg
+                  class="h-4 w-4 fill-blue-900"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path
+                    stroke-width="2"
+                    d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"
+                  />
+                </svg>
+              </label>
+
+              <div
+                id="sortboxmenu"
+                class="absolute mt-1 right-1 top-full min-w-max shadow rounded opacity-0 bg-gray-300 border border-gray-400 transition delay-75 ease-in-out z-10"
+              >
+                <ul class="block text-right text-gray-900">
+                  <li><a href="#" class="block px-3 py-2 hover:bg-gray-200">labaratory</a></li>
+                  <li><a href="#" class="block px-3 py-2 hover:bg-gray-200">Physiotheraphy</a></li>
+                  <li>
+                    <a href="#" class="block px-3 py-2 hover:bg-gray-200">Gnaecology</a>
+                  </li>
+                  <li>
+                    <a href="#" class="block px-3 py-2 hover:bg-gray-200">ENT</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </a>
+        </div>
+
+        <div class="flex">
+          <a href="">
+            <svg
+              class="h-8 w-10 fill-blue-500"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 384 512"
+            >
+              <path
+                d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"
+              />
+            </svg>
+            <p class="ml-16 truncate text-sm font-medium text-gray-900">Location</p>
+            <p class="ml-16 truncate text-sm font-medium text-gray-800">New York, US</p>
+          </a>
+        </div>
+      </div>
+   
+  </section>
 </template>
+
+
